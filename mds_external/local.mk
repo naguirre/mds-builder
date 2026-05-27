be9040c12abe7a269ef0c4884ca1bf0669389148
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2026-05-27 20:43:47 +0200

    build: stop tracking local.mk so CI uses upstream sources

--- mds_external/local.mk
+++
@@ -1,6 +0,0 @@
-ESP_HOSTED_OVERRIDE_SRCDIR = $(TOPDIR)/../../src/esp-hosted
-LINUX_OVERRIDE_SRCDIR = $(TOPDIR)/../../build/linux
-# MESA3D_OVERRIDE_SRCDIR = $(TOPDIR)/../../src/mesa
-#SQUEEZEPLAY_OVERRIDE_SRCDIR = $(TOPDIR)/../../src/squeezeos-squeezeplay/src/squeezeplay
-#TOLUA_OVERRIDE_SRCDIR = $(TOPDIR)/../../src/squeezeos-squeezeplay/src/tolua++-1.0.92/
-#SDL_OVERRIDE_SRCDIR = $(TOPDIR)/../../src/squeezeos-squeezeplay/src/sdl-1.2.15
